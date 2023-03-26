65231a9eaca60e7482224a76258d9e3d7182321f
Andrian215 <andrej.shand782@gmail.com>
2023-03-27 02:20:00 +0300

    Maze
    Maze

--- IndividualTasks/Maze/Assets/Scripts/Door.cs
+++ IndividualTasks/Maze/Assets/Scripts/Door.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public Key key; // Посилання на скрипт ключа
+    public Key key; 
 
     private void OnTriggerEnter(Collider other)
     {
-        // Перевірка, чи гравець зіштовхнувся з дверима
         if (other.CompareTag("MainCamera"))
         {
-            // Перевірка, чи гравець має ключ
             if (key.hasKey)
             {
-                // Якщо ключ є, то знищуємо двері
                 Destroy(gameObject);
                 Debug.Log("Labyrinth completed, Congrats!");
             }
